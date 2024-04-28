78b03e370246c5f680da35a446d95356d771a20c
Gwynne Raskind <gwynne@vapor.codes>
2024-04-28 13:13:48 -0500

    Bump minimum Swift version to 5.8 and switch to released version of SQLKit

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
     name: "QueuesFluentDriver",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
     ],
     products: [
         .library(name: "QueuesFluentDriver", targets: ["QueuesFluentDriver"]),
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.92.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.45.1"),
-        .package(url: "https://github.com/vapor/sql-kit.git", branch: "overhaul"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.29.2"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.13.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
     ],
@@ -27,7 +27,8 @@ let package = Package(
                 .product(name: "FluentSQL", package: "fluent-kit"),
                 .product(name: "SQLKit", package: "sql-kit"),
                 .product(name: "Queues", package: "queues")
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
         .testTarget(
             name: "QueuesFluentDriverTests",
@@ -35,7 +36,16 @@ let package = Package(
                 .product(name: "XCTVapor", package: "vapor"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .target(name: "QueuesFluentDriver"),
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
     ]
 )
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ForwardTrailingClosures"),
+    .enableUpcomingFeature("ConciseMagicFile"),
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableUpcomingFeature("StrictConcurrency"),
+    .enableExperimentalFeature("StrictConcurrency=complete"),
+] }
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.92.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.45.1"),
-        .package(url: "https://github.com/vapor/sql-kit.git", branch: "overhaul"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.29.2"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.13.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
     ],
