ebe058e4fe42289d99bb599cbfbcaaab59524878
Gwynne Raskind <gwynne@vapor.codes>
2024-06-14 01:37:29 -0500

    Completely overhaul the tests - they're now even more async and will test all database drivers that are available at the time the tests are built. Also use ConsoleKit's logger instead of StreamLogHandler because it's way more readable.

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -18,6 +18,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.48.4"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.30.0"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.15.0"),
+        .package(url: "https://github.com/vapor/console-kit.git", from: "4.14.3"),
         //.package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.7.1"),
         //.package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.9.1"),
         //.package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.5.0"),
@@ -39,6 +40,7 @@ let package = Package(
             name: "QueuesFluentDriverTests",
             dependencies: [
                 .product(name: "XCTVapor", package: "vapor"),
+                .product(name: "ConsoleKitTerminal", package: "console-kit"),
                 //.product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 //.product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 //.product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
