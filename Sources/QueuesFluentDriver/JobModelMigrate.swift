4bdeef68b2bd8aff397c3cb962b8d630dc20f676
Gwynne Raskind <gwynne@vapor.codes>
2025-03-30 15:59:27 -0500

    Fix use of non-default spaces in the JobModelMigration so the enum type is created in the same space as the table.

--- Sources/QueuesFluentDriver/JobModelMigrate.swift
+++ Sources/QueuesFluentDriver/JobModelMigrate.swift
@@ -1,34 +1,37 @@
 import SQLKit
 
 public struct JobModelMigration: AsyncSQLMigration {
-    private let jobsTableString: String
     private let jobsTable: SQLQualifiedTable
+    private let jobsTableIndexName: String
+    private let stateEnumType: SQLQualifiedTable // "table" is a bit of a misnomer here, but the expression still does the right thing
+    private let oldStateEnumName: String?
 
     /// Public initializer.
     public init(
         jobsTableName: String = "_jobs_meta",
         jobsTableSpace: String? = nil
     ) {
-        self.jobsTableString = "\(jobsTableSpace.map { "\($0)_" } ?? "")\(jobsTableName)"
-        self.jobsTable = .init(jobsTableName, space: jobsTableSpace)
+        self.jobsTable = SQLQualifiedTable(jobsTableName, space: jobsTableSpace)
+        self.jobsTableIndexName = "i_\(jobsTableName)_state_queue_delayuntil"
+        self.stateEnumType = SQLQualifiedTable("\(jobsTableName)_storedjobstatus", space: jobsTableSpace)
+        self.oldStateEnumName = jobsTableSpace.map { "\($0)_\(jobsTableName)_storedjobstatus" }
     }
 
     // See `AsyncSQLMigration.prepare(on:)`.
     public func prepare(on database: any SQLDatabase) async throws {
-        let stateEnumType: any SQLExpression
+        let actualStateEnumType: SQLDataType
 
         switch database.dialect.enumSyntax {
         case .typeName:
-            stateEnumType = .identifier("\(self.jobsTableString)_storedjobstatus")
-            var builder = database.create(enum: stateEnumType)
+            var builder = database.create(enum: self.stateEnumType)
             builder = StoredJobState.allCases.reduce(builder, { $0.value($1.rawValue) })
             try await builder.run()
+            actualStateEnumType = .custom(self.stateEnumType)
         case .inline:
-             // This is technically a misuse of SQLFunction, but it produces the correct syntax
-            stateEnumType = .function("enum", StoredJobState.allCases.map { .literal($0.rawValue) })
+            actualStateEnumType = .custom(SQLEnumDataType(cases: StoredJobState.allCases.map { .literal($0.rawValue) }))
         default:
             // This is technically a misuse of SQLFunction, but it produces the correct syntax
-            stateEnumType = .function("varchar", .literal(16))
+            actualStateEnumType = .custom(.function("varchar", .literal(16)))
         }
 
         /// This whole pile of nonsense is only here because of
@@ -46,18 +49,18 @@ public struct JobModelMigration: AsyncSQLMigration {
         }
 
         try await database.create(table: self.jobsTable)
-            .column("id",              type: .text,                  .primaryKey(autoIncrement: false))
-            .column("queue_name",      type: .text,                  .notNull)
-            .column("job_name",        type: .text,                  .notNull)
-            .column("queued_at",       type: manualTimestampType,    .notNull)
-            .column("delay_until",     type: manualTimestampType,    .default(SQLLiteral.null))
-            .column("state",           type: .custom(stateEnumType), .notNull)
-            .column("max_retry_count", type: .int,                   .notNull)
-            .column("attempts",        type: .int,                   .notNull)
-            .column("payload",         type: .blob,                  .notNull)
-            .column("updated_at",      type: .timestamp,             autoTimestampConstraints)
+            .column("id",              type: .text,               .primaryKey(autoIncrement: false))
+            .column("queue_name",      type: .text,               .notNull)
+            .column("job_name",        type: .text,               .notNull)
+            .column("queued_at",       type: manualTimestampType, .notNull)
+            .column("delay_until",     type: manualTimestampType, .default(SQLLiteral.null))
+            .column("state",           type: actualStateEnumType, .notNull)
+            .column("max_retry_count", type: .int,                .notNull)
+            .column("attempts",        type: .int,                .notNull)
+            .column("payload",         type: .blob,               .notNull)
+            .column("updated_at",      type: .timestamp,          autoTimestampConstraints)
             .run()
-        try await database.create(index: "i_\(self.jobsTableString)_state_queue_delayUntil")
+        try await database.create(index: self.jobsTableIndexName)
             .on(self.jobsTable)
             .column("state")
             .column("queue_name")
@@ -68,9 +71,17 @@ public struct JobModelMigration: AsyncSQLMigration {
     // See `AsyncSQLMigration.revert(on:)`.
     public func revert(on database: any SQLDatabase) async throws {
         try await database.drop(table: self.jobsTable).run()
+
         switch database.dialect.enumSyntax {
         case .typeName:
-            try await database.drop(enum: "\(self.jobsTableString)_storedjobstatus").run()
+            // In version 3.0.1 and earlier of the driver, if a space was specified, the enum's name was different,
+            // and the enum ended up in the default space rather than the one given. For compatibility, we need to drop
+            // the old name from the default space. Fortunately, `DROP TYPE` supports `IF EXISTS`.
+            if let oldStateEnumName = self.oldStateEnumName {
+                try await database.drop(enum: oldStateEnumName).ifExists().run()
+            }
+
+            try await database.drop(enum: self.stateEnumType).ifExists().run()
         default:
             break
         }
